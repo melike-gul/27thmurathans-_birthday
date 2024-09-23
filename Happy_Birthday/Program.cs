b3dd0666e052116b8ef8b686491ce75e0e0be2a3
melike-gul <melikegul005@gmail.com>
2024-09-23 19:14:05 +0300

    :)

---
+++ Happy_Birthday/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Happy_Birthday;
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public class Kisi
+    {
+        public string Ad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+    }
+
+    static void Main()
+    {
+        List<Kisi> kisiler = new List<Kisi>
+        {
+            new Kisi { Ad = "Murathan Saygılı", DogumTarihi = new DateTime(1997, 9, 23) }
+        };
+
+        DateTime bugun = DateTime.Now;
+
+        foreach (var kisi in kisiler)
+        {
+            if (kisi.DogumTarihi.Month == bugun.Month && kisi.DogumTarihi.Day == bugun.Day)
+            {
+                Console.WriteLine($"Doğum Günün Kutlu Olsun {kisi.Ad}!");
+            }
+        }
+    }
+}
+
